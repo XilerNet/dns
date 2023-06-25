ed8b9430e4781562f5e423704f1a4d48cffef682
Arthurdw <dev@arthurdw.com>
2023-06-25 13:22:11 +0200

    :recycle: Formatted

--- crates/shared/src/time.rs
+++ crates/shared/src/time.rs
@@ -1,5 +1,14 @@
 use std::time::SystemTime;
 
+/// Converts a u64 integer representing seconds since the Unix epoch to a `SystemTime` object.
+///
+/// # Arguments
+///
+/// * `seconds` - The number of seconds since the Unix epoch.
+///
+/// # Returns
+///
+/// A `SystemTime` object representing the timestamp.
 pub fn system_time_from_epoch_seconds(seconds: u64) -> SystemTime {
     SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(seconds)
 }
